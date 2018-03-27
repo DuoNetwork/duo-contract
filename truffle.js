88893df6a4355cf44b58909ffa628c0b760b2a7c
yizhoucao <yizhou.cao@finbook.co>
2018-03-27 14:37:56 +0800

    update network config

--- truffle.js
+++ truffle.js
@@ -1,7 +1,19 @@
 module.exports = {
 	migrations_directory: './migrations',
 	networks: {
-		development: {
+		test: {
+			host: 'localhost',
+			port: 8545,
+			network_id: '*' // Match any network id,
+			// gas:5000000
+		},
+		kovan: {
+			host: 'localhost',
+			port: 8545,
+			network_id: '*' // Match any network id,
+			// gas:5000000
+		},
+		live: {
 			host: 'localhost',
 			port: 8545,
 			network_id: '*' // Match any network id,
