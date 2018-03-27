3f1d8592dbd4f53020476386e5eeb2ba48a8ef70
WYANG008 <weitao.yang@finbook.co>
2018-03-27 15:48:51 +0800

    add all view methods in custodianMock

--- contracts/CustodianMock.sol
+++ contracts/CustodianMock.sol
@@ -41,7 +41,35 @@ contract CustodianMock is Custodian {
 
 	}
 
+	function getFeeCollector() public view returns (address _feeCollector) {
+		return feeCollector;
+	}
 
+	function getPriceFeed1() public view returns (address _priceFeed1) {
+		return priceFeed1;
+	}
+	function getPriceFeed2() public view returns (address _priceFeed2) {
+		return priceFeed2;
+	}
+	function getPriceFeed3() public view returns (address _priceFeed3) {
+		return priceFeed3;
+	}
+	function getAdmin() public view returns (address _admin) {
+		return admin;
+	}
+
+	function getFirstAddr() public view returns (address _firstAddr) {
+		return firstAddr;
+	}
+	function getSecondAddr() public view returns (address _secondAddr) {
+		return secondAddr;
+	}
+	function getFirstPrice() public view returns (Price _firstPrice) {
+		return firstPrice;
+	}
+	function getSecondPrice() public view returns (Price _secondPrice) {
+		return secondPrice;
+	}
 
 	function getPriceFeedTolInBP() public view returns (uint256 _priceFeedTolInBP) {
 		return priceFeedTolInBP;
@@ -49,6 +77,40 @@ contract CustodianMock is Custodian {
 
 	function getFeeAccumulatedInWei() public view returns (uint256 _feeAccumulatedInWei) {
 		return feeAccumulatedInWei;
+	}
+
+	function getPreResetWaitingBlocks() public view returns (uint256 _preResetWaitingBlocks) {
+		return preResetWaitingBlocks;
+	}
+
+	function getPostResetWaitingBlocks() public view returns (uint256 _postResetWaitingBlocks) {
+		return postResetWaitingBlocks;
+	}
+
+	function getPriceTolInBP() public view returns (uint256 _priceTolInBP) {
+		return priceTolInBP;
+	}
+	function getPriceFeedTimeTol() public view returns (uint256 _priceFeedTimeTol) {
+		return priceFeedTimeTol;
+	}
+
+	function getPriceUpdateCoolDown() public view returns (uint256 _priceUpdateCoolDown) {
+		return priceUpdateCoolDown;
+	}
+
+	function getNumOfPrices() public view returns (uint256 _numOfPrices) {
+		return numOfPrices;
+	}
+	function getLastPreResetBlockNo() public view returns (uint256 _lastPreResetBlockNo) {
+		return lastPreResetBlockNo;
+	}
+
+	function getLastPostResetBlockNo() public view returns (uint256 _lastPostResetBlockNo) {
+		return lastPostResetBlockNo;
+	}
+
+	function getNextResetAddrIndex() public view returns (uint256 _nextResetAddrIndex) {
+		return nextResetAddrIndex;
 	}
 
 }
