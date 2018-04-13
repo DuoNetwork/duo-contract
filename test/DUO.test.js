4b613bac550b6869b272318d3124ba665ba89493
yizhoucao <yizhou.cao@finbook.co>
2018-04-13 18:45:55 +0800

    use async awaits for DUO.test

--- test/DUO.test.js
+++ test/DUO.test.js
@@ -10,139 +10,106 @@ contract('DUO', accounts => {
 	const WEI_DENOMINATOR = 1e18;
 	const TOTAL_SUPPLY = 10000;
 
-	before(() =>
-		DUO.new(web3.utils.toWei(TOTAL_SUPPLY + ''), 'DUO', 'DUO', { from: creator }).then(
-			instance => (duoContract = instance)
-		)
+	before(
+		async () =>
+			(duoContract = await DUO.new(web3.utils.toWei(TOTAL_SUPPLY + ''), 'DUO', 'DUO', {
+				from: creator
+			}))
 	);
 
-	it('total supply should be 10000', () => {
-		return duoContract.totalSupply.call().then(supply => {
-			return assert.equal(
-				// web3 1.0 has BN package for fromWei/toWei and only takes in string or BN object
-				// but the conversion does not handle scientific notation of 1e+22
-				supply.toNumber() / WEI_DENOMINATOR,
-				10000,
-				'totalSupply not equal to 10000'
-			);
-		});
+	it('total supply should be 10000', async () => {
+		let supply = await duoContract.totalSupply.call();
+		assert.equal(
+			// web3 1.0 has BN package for fromWei/toWei and only takes in string or BN object
+			// but the conversion does not handle scientific notation of 1e+22
+			supply.toNumber() / WEI_DENOMINATOR,
+			10000,
+			'totalSupply not equal to 10000'
+		);
 	});
 
-	it('creator balance should be 10000', () => {
-		return duoContract.balanceOf
-			.call(creator)
-			.then(balance =>
-				assert.equal(
-					balance.toNumber() / WEI_DENOMINATOR,
-					10000,
-					'balance of owner not equal to 10000'
-				)
-			);
+	it('creator balance should be 10000', async () => {
+		let balance = await duoContract.balanceOf.call(creator);
+		assert.equal(
+			balance.toNumber() / WEI_DENOMINATOR,
+			10000,
+			'balance of owner not equal to 10000'
+		);
 	});
 
-	it('non creater balance should be 0', () => {
-		return duoContract.balanceOf
-			.call(alice)
-			.then(balance =>
-				assert.equal(
-					balance.toNumber() / WEI_DENOMINATOR,
-					0,
-					'balance of owner not equal to 0'
-				)
-			);
+	it('non creater balance should be 0', async () => {
+		let balance = await duoContract.balanceOf.call(alice);
+		assert.equal(balance.toNumber() / WEI_DENOMINATOR, 0, 'balance of owner not equal to 0');
 	});
 
-	it('should be able to approve', () => {
-		return duoContract
-			.approve(alice, web3.utils.toWei('100'), { from: creator })
-			.then(success => {
-				assert.isTrue(!!success, 'Not able to approve');
-			});
+	it('should be able to approve', async () => {
+		let success = await duoContract.approve(alice, web3.utils.toWei('100'), { from: creator });
+		assert.isTrue(!!success, 'Not able to approve');
 	});
 
-	it('should show allowance', () => {
-		return duoContract.allowance.call(creator, alice).then(allowance => {
+	it('should show allowance', async () => {
+		let allowance = await duoContract.allowance.call(creator, alice);
+		assert.equal(
+			allowance.toNumber() / WEI_DENOMINATOR,
+			100,
+			'allowance of alice not equal to 100'
+		);
+	});
+
+	it('creator should be able to transfer to bob', async () => {
+		let transfer = await duoContract.transfer(bob, web3.utils.toWei('10'), { from: creator });
+		assert.isTrue(!!transfer, 'Not able to approve');
+	});
+
+	it('should show balance of bob', async () => {
+		let balance = await duoContract.balanceOf.call(bob);
+		assert.equal(balance.toNumber() / WEI_DENOMINATOR, 10, 'balance of bob not equal to 10');
+	});
+
+	it('alice cannot transfer 200 from creator to bob', async () => {
+		try {
+			await duoContract.transferFrom(creator, bob, web3.utils.toWei('200'), { from: alice });
+			assert.isTrue(false, 'can transfer of more than balance');
+		} catch (err) {
 			assert.equal(
-				allowance.toNumber() / WEI_DENOMINATOR,
-				100,
-				'allowance of alice not equal to 100'
+				err.message,
+				'VM Exception while processing transaction: revert',
+				'transaction not reverted'
 			);
+		}
+	});
+
+	it('alice should transfer 50 from creator to bob', async () => {
+		let transferFrom = await duoContract.transferFrom(creator, bob, web3.utils.toWei('50'), {
+			from: alice
 		});
+		assert.isTrue(!!transferFrom, 'Not able to transferFrom');
 	});
 
-	it('creator should be able to transfer to bob', () => {
-		return duoContract
-			.transfer(bob, web3.utils.toWei('10'), { from: creator })
-			.then(transfer => {
-				assert.isTrue(!!transfer, 'Not able to approve');
-			});
+	it('allowance for alice should be 50', async () => {
+		let allowance = await duoContract.allowance.call(creator, alice);
+		assert.equal(
+			allowance.toNumber() / WEI_DENOMINATOR,
+			50,
+			'allowance of alice not equal to 50'
+		);
 	});
 
-	it('should show balance of bob', () => {
-		return duoContract.balanceOf
-			.call(bob)
-			.then(balance =>
-				assert.equal(
-					balance.toNumber() / WEI_DENOMINATOR,
-					10,
-					'balance of bob not equal to 10'
-				)
-			);
+	it('check balance of bob equal 60', async () => {
+		let balance = await duoContract.balanceOf.call(bob);
+		assert.equal(balance.toNumber() / WEI_DENOMINATOR, 60, 'balance of bob not equal to 60');
 	});
 
-	it('alice cannot transfer 200 from creator to bob', () => {
-		return duoContract
-			.transferFrom(creator, bob, web3.utils.toWei('200'), { from: alice })
-			.then(() => assert.isTrue(false, 'can transfer of more than balance'))
-			.catch(err =>
-				assert.equal(
-					err.message,
-					'VM Exception while processing transaction: revert',
-					'transaction not reverted'
-				)
-			);
-	});
-
-	it('alice should transfer 50 from creator to bob', () => {
-		return duoContract
-			.transferFrom(creator, bob, web3.utils.toWei('50'), { from: alice })
-			.then(transferFrom => {
-				assert.isTrue(!!transferFrom, 'Not able to transferFrom');
-			});
-	});
-
-	it('allowance for alice should be 50', () => {
-		return duoContract.allowance.call(creator, alice).then(allowance => {
+	it('should not transfer more than balance', async () => {
+		try {
+			await duoContract.transfer(bob, web3.utils.toWei('10000'), { from: creator });
+			assert.isTrue(false, 'can transfer of more than balance');
+		} catch (err) {
 			assert.equal(
-				allowance.toNumber() / WEI_DENOMINATOR,
-				50,
-				'allowance of alice not equal to 50'
+				err.message,
+				'VM Exception while processing transaction: revert',
+				'transaction not reverted'
 			);
-		});
-	});
-
-	it('check balance of bob equal 60', () => {
-		return duoContract.balanceOf
-			.call(bob)
-			.then(balance =>
-				assert.equal(
-					balance.toNumber() / WEI_DENOMINATOR,
-					60,
-					'balance of bob not equal to 60'
-				)
-			);
-	});
-
-	it('should not transfer more than balance', () => {
-		return duoContract
-			.transfer(bob, web3.utils.toWei('10000'), { from: creator })
-			.then(() => assert.isTrue(false, 'can transfer of more than balance'))
-			.catch(err =>
-				assert.equal(
-					err.message,
-					'VM Exception while processing transaction: revert',
-					'transaction not reverted'
-				)
-			);
+		}
 	});
 });
