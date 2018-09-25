4813e8635edc633735351a06562d6fc126bc798e
yizhoucao <yizhou.cao@finbook.co>
2018-09-25 13:54:27 +0800

    reviewed managed

--- test/Managed.test.js
+++ test/Managed.test.js
@@ -1,6 +1,5 @@
 const Custodian = artifacts.require('../contracts/custodians/CustodianMock.sol');
 const RoleManager = artifacts.require('../contracts/common/EsplanadeMock.sol');
-// const Magi = artifacts.require('../contracts/oracles/MagiMock.sol');
 const DUO = artifacts.require('../contracts/tokens/DuoMock.sol');
 const Managed = artifacts.require('../contracts/common/Managed.sol');
 const Web3 = require('web3');
@@ -11,40 +10,28 @@ const BeethovenInit = InitParas['Beethoven'];
 const DuoInit = InitParas['DUO'];
 const RoleManagerInit = InitParas['RoleManager'];
 const Pool = InitParas['Pool'];
-// const MagiInit = InitParas['Magi'];
 const ManagedInit = InitParas['Managed'];
 
 const VM_REVERT_MSG = 'VM Exception while processing transaction: revert';
 
 const EVENT_UPDATE_ROLEMANAGER = 'UpdateRoleManager';
-const EVENT_UPDATEOPERATOR = 'UpdateOperator';
+const EVENT_UPDATE_OPERATOR = 'UpdateOperator';
 
 let validColdPool = Pool[0].map(addr => web3.utils.toChecksumAddress(addr));
 
-contract('Managed', accounts => {
-	let managedContract; // duoContract;
-	let roleManagerContract; // managedContract;
+contract.only('Managed', accounts => {
+	let managedContract;
+	let roleManagerContract;
 
 	const creator = accounts[0];
 	const fc = accounts[1];
-	// const pf1 = accounts[2];
-	// const pf2 = accounts[3];
-	// const pf3 = accounts[4];
 	const alice = accounts[5];
 	const bob = accounts[6];
-	// const charles = accounts[7];
-	// const david = accounts[8];
-	// const eric = accounts[9];
-	// const frank = accounts[10];
-	// const newModerator = accounts[11];
-	// const newModerator2 = accounts[12];
 
 	const initContracts = async () => {
 		roleManagerContract = await RoleManager.new(RoleManagerInit.optCoolDown, {
 			from: creator
 		});
-		// custodianContract = await initCustodian();
-		// oracleContract = await initOracle();
 		managedContract = await initManaged();
 	};
 
@@ -79,48 +66,35 @@ contract('Managed', accounts => {
 		});
 	};
 
-	// const initOracle = async () => {
-	// 	return await Magi.new(
-	// 		creator,
-	// 		pf1,
-	// 		pf2,
-	// 		pf3,
-	// 		roleManagerContract.address,
-	// 		MagiInit.pxCoolDown,
-	// 		MagiInit.optCoolDown,
-	// 		{
-	// 			from: creator
-	// 		}
-	// 	);
-	// };
-
 	describe('constructor', () => {
 		before(initContracts);
+
 		it('should set roleManagerAddress correctly', async () => {
 			let value = await managedContract.roleManagerAddress.call();
 			assert.equal(
 				value.valueOf(),
 				roleManagerContract.address,
-				'rolemanagerAddr is not not started'
+				'rolemanagerAddr is not set'
 			);
 		});
 
-		it('should set opr correctly', async () => {
+		it('should set opt correctly', async () => {
 			let value = await managedContract.operator.call();
-			assert.equal(value.valueOf(), creator, 'opt is not not started');
+			assert.equal(value.valueOf(), creator, 'opt is not dry');
 		});
 
 		it('should set optCoolDown correctly', async () => {
 			let value = await managedContract.operationCoolDown.call();
-			assert.equal(value.valueOf(), ManagedInit.optCoolDown + '', 'optCD is not not started');
+			assert.equal(value.valueOf(), ManagedInit.optCoolDown + '', 'optCD is not set');
 		});
 	});
 
 	describe('updateRoleManager', () => {
 		before(initContracts);
-		it('should not set un passed rolemanager contract', async () => {
+		it('should not set not passed rolemanager contract', async () => {
 			try {
 				await managedContract.updateRoleManager.call(roleManagerContract.address);
+				assert.isTrue(false, 'can set not passed role manager contract');
 			} catch (err) {
 				assert.equal(err.message, VM_REVERT_MSG, 'not reverted');
 			}
@@ -130,6 +104,7 @@ contract('Managed', accounts => {
 			await roleManagerContract.setPassedContract(alice);
 			try {
 				await managedContract.updateRoleManager.call(alice);
+				assert.isTrue(false, 'can set role manager contract without moderator');
 			} catch (err) {
 				assert.equal(err.message, VM_REVERT_MSG, 'not reverted');
 			}
@@ -140,20 +115,16 @@ contract('Managed', accounts => {
 				from: creator
 			});
 			await roleManagerContract.setPassedContract(newRoleManagerContract.address);
-			// try {
+
 			let tx = await managedContract.updateRoleManager(newRoleManagerContract.address);
-
-			let roleManagerAddress = await managedContract.roleManagerAddress.call();
-			assert.isTrue(roleManagerAddress.valueOf() === newRoleManagerContract.address);
-			assert.isTrue(tx.logs.length === 1 && tx.logs[0].event === EVENT_UPDATE_ROLEMANAGER);
-
 			assert.isTrue(
-				tx.logs[0].args.newManagerAddress.valueOf() === newRoleManagerContract.address,
-				'wrong event argument'
+				tx.logs.length === 1 &&
+					tx.logs[0].event === EVENT_UPDATE_ROLEMANAGER &&
+					tx.logs[0].args.newManagerAddress === newRoleManagerContract.address
 			);
 		});
 
-		it('should not update within cooldow', async () => {
+		it('should not update within cooldown', async () => {
 			let new2RoleManagerContract = await RoleManager.new(RoleManagerInit.optCoolDown, {
 				from: creator
 			});
@@ -170,18 +141,17 @@ contract('Managed', accounts => {
 	describe('updateOperator', () => {
 		before(initContracts);
 		let custodianContract;
-		it('non cold address cannot upate', async () => {
+		it('address not in pool cannot update', async () => {
 			try {
-				await managedContract.updateOperator();
-				assert.isTrue(false, 'non hot addr can update');
+				await managedContract.updateOperator({ from: alice });
+				assert.isTrue(false, 'address not in pool can update');
 			} catch (err) {
 				assert.equal(err.message, VM_REVERT_MSG, 'not reverted');
 			}
 		});
 
-		it('cold address can upate', async () => {
+		it('cold address can update', async () => {
 			await roleManagerContract.setPool(0, 0, alice);
-			// try{
 			custodianContract = await initCustodian();
 			await roleManagerContract.addCustodian(custodianContract.address, {
 				from: creator
@@ -189,7 +159,7 @@ contract('Managed', accounts => {
 			await roleManagerContract.skipCooldown(1);
 			let tx = await custodianContract.updateOperator({ from: alice });
 			assert.isTrue(
-				tx.logs.length === 1 && tx.logs[0].event === EVENT_UPDATEOPERATOR,
+				tx.logs.length === 1 && tx.logs[0].event === EVENT_UPDATE_OPERATOR,
 				'event wrong'
 			);
 
@@ -203,12 +173,12 @@ contract('Managed', accounts => {
 			);
 		});
 
-		it('colld address cannot update can upate', async () => {
-			await roleManagerContract.setPool(0, 0, bob);
+		it('hot address cannot update ', async () => {
+			await roleManagerContract.setPool(1, 0, bob);
 			await roleManagerContract.skipCooldown(1);
 			try {
 				await custodianContract.updateOperator({ from: bob });
-				assert.isTrue(false, 'non hot addr can update');
+				assert.isTrue(false, 'hot addr can update');
 			} catch (err) {
 				assert.equal(err.message, VM_REVERT_MSG, 'not reverted');
 			}
