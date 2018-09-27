7234a50e01c3d40d613e9aa903119ebecf60f676
yizhoucao <yizhou.cao@finbook.co>
2018-09-27 10:14:12 +0800

    skip mocks

--- .solcover.js
+++ .solcover.js
@@ -1,5 +1,6 @@
 module.exports = {
 	norpc: true,
+	skipFiles: ['mocks/BeethovenMock.sol', 'mocks/CustodianMock.sol', 'mocks/DUOMock.sol', 'mocks/EsplanadeMock.sol', 'mocks/MagiMock.sol', 'mocks/WETHMock.sol'],
 	compileCommand: '..\\node_modules\\.bin\\truffle.cmd compile --network coverage',
 	testCommand: '..\\node_modules\\.bin\\truffle.cmd test --network coverage'
 };
