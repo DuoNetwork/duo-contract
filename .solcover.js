891fcbf02f346362e76365c31302bc5774512449
yizhoucao <yizhou.cao@finbook.co>
2019-02-18 16:26:18 +0800

    update coverage config

--- .solcover.js
+++ .solcover.js
@@ -1,6 +1,24 @@
 module.exports = {
 	norpc: true,
-	skipFiles: ['mocks/BeethovenMock.sol', 'mocks/CustodianMock.sol', 'mocks/CustodianTokenMock.sol', 'mocks/DualClassCustodianMock.sol', 'mocks/DUOMock.sol', 'mocks/Erc20CustodianMock.sol', 'mocks/EsplanadeMock.sol', 'mocks/MagiMock.sol', 'mocks/MozartMock', 'mocks/OptionCustodianMock', 'mocks/WETHMock.sol'],
+	skipFiles: [
+		'interfaces/ICustodian.sol',
+		'interfaces/ICustodianToken.sol',
+		'interfaces/IERC20.sol',
+		'interfaces/IMultiSigManager.sol',
+		'interfaces/IOracle.sol',
+		'interfaces/IWETH.sol',
+		'mocks/BeethovenMock.sol',
+		'mocks/CustodianMock.sol',
+		'mocks/CustodianTokenMock.sol',
+		'mocks/DualClassCustodianMock.sol',
+		'mocks/DUOMock.sol',
+		'mocks/Erc20CustodianMock.sol',
+		'mocks/EsplanadeMock.sol',
+		'mocks/MagiMock.sol',
+		'mocks/MozartMock',
+		'mocks/OptionCustodianMock',
+		'mocks/WETHMock.sol'
+	],
 	compileCommand: 'truffle compile --network coverage',
 	testCommand: 'truffle test --network coverage'
 };
