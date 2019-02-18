231da2c851655aed139f8f96a14e6c3b7e3ce6c9
yizhoucao <yizhou.cao@finbook.co>
2019-02-18 15:58:22 +0800

    update coverage config

--- .solcover.js
+++ .solcover.js
@@ -1,6 +1,6 @@
 module.exports = {
 	norpc: true,
-	skipFiles: ['mocks/BeethovenMock.sol', 'mocks/CustodianMock.sol', 'mocks/DUOMock.sol', 'mocks/EsplanadeMock.sol', 'mocks/MagiMock.sol', 'mocks/WETHMock.sol'],
-	compileCommand: '..\\node_modules\\.bin\\truffle.cmd compile --network coverage',
-	testCommand: '..\\node_modules\\.bin\\truffle.cmd test --network coverage'
+	skipFiles: ['mocks/BeethovenMock.sol', 'mocks/CustodianMock.sol', 'mocks/CustodianTokenMock.sol', 'mocks/DualClassCustodianMock.sol', 'mocks/DUOMock.sol', 'mocks/Erc20CustodianMock.sol', 'mocks/EsplanadeMock.sol', 'mocks/MagiMock.sol', 'mocks/MozartMock', 'mocks/OptionCustodianMock', 'mocks/WETHMock.sol'],
+	compileCommand: 'truffle compile --network coverage',
+	testCommand: 'truffle test --network coverage'
 };
