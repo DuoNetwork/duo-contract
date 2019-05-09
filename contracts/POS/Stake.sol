a01c43cd9d57d71d6a543bf63b3b990c53790f25
WYANG008 <weitao.yang@finbook.co>
2019-05-09 16:25:35 +0800

    add unstake

--- contracts/POS/Stake.sol
+++ contracts/POS/Stake.sol
@@ -8,16 +8,30 @@ import { IERC20 } from "../interfaces/IERC20.sol";
 contract Stake {
 	using SafeMath for uint;
 
+	struct QueueIdx {
+		uint first;
+		uint last;
+	}
+
+	struct Stake {
+		address pf;
+		uint timestamp;
+		uint amtInWei;
+	}
+
 	/*
      * Storage
      */
 	address public duoTokenAddress;
 	IERC20 duoTokenContract;
+	uint lockMinTimeInSecond;
 
 	// uint public totalFeeders;
 	// committers
 	mapping (address => bool) public isWhiteListCommitter;
-	mapping (address => mapping (address => uint)) public stakeInWei;
+	
+	mapping (address => QueueIdx) public userQueueIdx;
+	mapping (address => mapping (uint => Stake)) public userStakeQueue;
 
 	/*
      * Modifier
@@ -30,14 +44,15 @@ contract Stake {
 	/*
      * Events
      */
-	event Stake(address indexed from, address indexed pf, uint amtInWei);
-
+	event AddStake(address indexed from, address indexed pf, uint amtInWei);
+	event UnStake(address indexed from, address indexed pf, uint amtInWei);
 	/*
      * Constructor
      */
 	constructor(
 		address duoTokenAddr,
-		address[] memory pfList
+		address[] memory pfList,
+		uint lockTime
 		) 
 		public
 	{
@@ -46,16 +61,32 @@ contract Stake {
 		for(uint i= 0; i <pfList.length; i++){
 			isWhiteListCommitter[pfList[i]] = true;
 		}
+		lockMinTimeInSecond = lockTime;
+
 	}
 
 
 	/*
      * Public Functions
      */
-	function stake(address addr, uint amtInWei) public isPriceFeed(addr) returns(bool){
+	function addStake(address addr, uint amtInWei) public isPriceFeed(addr) returns(bool){
 		address sender = msg.sender;
 		require(duoTokenContract.transferFrom(sender, address(this), amtInWei));
-		stakeInWei[sender][addr] = stakeInWei[sender][addr].add(amtInWei);
+		userQueueIdx[sender].last +=1;
+		userStakeQueue[sender][userQueueIdx[sender].last] = Stake(addr, block.timestamp, amtInWei);
+		emit AddStake(sender, addr, amtInWei);
 		return true;
 	}
+
+	function unStake(address addr) public returns(bool) {
+		address sender = msg.sender;
+		require(userQueueIdx[sender].last >= userQueueIdx[sender].first);  // non-empty queue
+		Stake memory stake = userStakeQueue[sender][userQueueIdx[sender].first];
+		require(block.timestamp.sub(stake.timestamp).sub(lockMinTimeInSecond) > 0); 
+		delete userStakeQueue[sender][userQueueIdx[sender].first];
+		userQueueIdx[sender].first += 1;
+		emit UnStake(sender, stake.pf, stake.amtInWei);
+		return true;
+	}
+
 }
--- test/Stake.test.js
+++ test/Stake.test.js
@@ -1,10 +1,13 @@
 const Stake = artifacts.require('../contracts/POS/Stake.sol');
 const DUO = artifacts.require('../contracts/tokens/DUO.sol');
-const CST = require('./constants');
+// const CST = require('./constants');
 const util = require('./util');
 
 const InitParas = require('../migrations/contractInitParas.json');
 const DuoInit = InitParas['DUO'];
+const StakeInit = InitParas['Stake'];
+
+const EVENT_ADD_STAKE = 'AddStake';
 
 contract('Stake', accounts => {
 	let duoContract, stakeContract;
@@ -15,7 +18,7 @@ contract('Stake', accounts => {
 	const pf3 = accounts[3];
 	const pfList = [pf1,pf2,pf3];
 	const nonPf = accounts[4];
-	const alice = accounts[5];
+	// const alice = accounts[5];
 
 
 	const initContracts = async () => {
@@ -30,8 +33,8 @@ contract('Stake', accounts => {
 
 		stakeContract = await Stake.new(
 			duoContract.address,
-			[pf1,pf2,pf3]
-			,
+			[pf1,pf2,pf3],
+			StakeInit.minStakeTs,
 			{
 				from: creator
 			}
@@ -54,29 +57,47 @@ contract('Stake', accounts => {
 		});
 	});
 
-	describe('stake', () => {
+	describe('addStake', () => {
 		before(initContracts);
 
-		it('cannot stake more than  balance', async () => {
-			try {
-				await stakeContract.stake(pf1, util.toWei(50), {
-					from: alice
-				});
-				assert.isTrue(false, 'can stake more than balance');
-			} catch (err) {
-				assert.equal(err.message, CST.VM_REVERT_MSG, 'transaction not reverted');
-			}
-		});
+		// it('cannot stake more than  balance', async () => {
+		// 	try {
+		// 		await stakeContract.stake(pf1, util.toWei(50), {
+		// 			from: alice
+		// 		});
+		// 		assert.isTrue(false, 'can stake more than balance');
+		// 	} catch (err) {
+		// 		assert.equal(err.message, CST.VM_REVERT_MSG, 'transaction not reverted');
+		// 	}
+		// });
 
-		it('cannot stake to not whitelisted addr', async () => {
-			try {
-				await stakeContract.stake(nonPf, util.toWei(50), {
-					from: creator
-				});
-				assert.isTrue(false, 'can stake to not whitelisted addr');
-			} catch (err) {
-				assert.equal(err.message, CST.VM_REVERT_MSG, 'transaction not reverted');
-			}
+		// it('cannot stake to not whitelisted addr', async () => {
+		// 	try {
+		// 		await stakeContract.stake(nonPf, util.toWei(50), {
+		// 			from: creator
+		// 		});
+		// 		assert.isTrue(false, 'can stake to not whitelisted addr');
+		// 	} catch (err) {
+		// 		assert.equal(err.message, CST.VM_REVERT_MSG, 'transaction not reverted');
+		// 	}
+		// });
+
+		it('can addStake', async () => {
+			await duoContract.approve(stakeContract.address, util.toWei(100));
+			
+			let tx = await stakeContract.addStake(pf1, util.toWei(50), {
+				from: creator
+			});
+			assert.isTrue(tx.logs.length ===1 && tx.logs[0].event === EVENT_ADD_STAKE, 'log events incorrect');
+
+
+			const queIdx = await stakeContract.userQueueIdx.call(creator);
+			assert.isTrue( 
+				util.isEqual(queIdx.first.valueOf(), 0) && 
+				util.isEqual(queIdx.last.valueOf(), 1),
+				"queueIndex not updated correctly"
+			);
+			
 		});
 
 	});
